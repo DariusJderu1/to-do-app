1f16f297f6c8e587ce22be0a22dc7fabd76c158c
Darius Jderu <jderudarius@gmail.com>
2026-06-27 18:31:38 +0300

    Add TodoService functionality

---
+++ backend/src/main/java/com/darius/todoapp/service/TodoService.java
@@ -0,0 +1,20 @@
+package com.darius.todoapp.service;
+
+import com.darius.todoapp.entity.Todo;
+
+import java.util.List;
+
+public interface TodoService {
+
+    List<Todo> findAll();
+
+    Todo save(Todo theTodo);
+
+    void deleteById(Long theId);
+
+    List<Todo> findTodosToday();
+
+    List<Todo> findTodosNextSevenDays();
+
+    List<Todo> findAllImportant();
+}
---
+++ backend/src/main/java/com/darius/todoapp/service/TodoServiceImpl.java
@@ -0,0 +1,63 @@
+package com.darius.todoapp.service;
+
+import com.darius.todoapp.dao.TodoDAO;
+import com.darius.todoapp.entity.Todo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class TodoServiceImpl implements TodoService{
+
+    private final TodoDAO todoDAO;
+
+
+    @Autowired
+    public TodoServiceImpl(TodoDAO todoDAO) {
+
+        this.todoDAO = todoDAO;
+    }
+
+
+    @Override
+    public List<Todo> findAll() {
+
+        return todoDAO.findAll();
+    }
+
+    @Transactional
+    @Override
+    public Todo save(Todo theTodo) {
+
+        return todoDAO.save(theTodo);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(Long theId) {
+
+        todoDAO.deleteById(theId);
+    }
+
+    @Override
+    public List<Todo> findTodosToday() {
+
+        return todoDAO.findByDueDate(LocalDate.now());
+    }
+
+    @Override
+    public List<Todo> findTodosNextSevenDays() {
+
+        LocalDate today = LocalDate.now();
+        return todoDAO.findByDueDateBetween(today, today.plusDays(7));
+    }
+
+    @Override
+    public List<Todo> findAllImportant() {
+
+        return todoDAO.findAllImportant();
+    }
+}
