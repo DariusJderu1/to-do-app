9116d452d40eb96a8313783d462e97e989523fea
Darius Jderu <jderudarius@gmail.com>
2026-07-02 15:57:14 +0300

    Add Header Component

--- frontend/src/components/app/Header.jsx
+++ frontend/src/components/app/Header.jsx
@@ -1,15 +1,16 @@
 import { RiMenuUnfold4Line } from "react-icons/ri";
+import { IoSettingsSharp } from "react-icons/io5";
 
 function Header() {
 
     return (
         
         <header>
-            <span>
-               <RiMenuUnfold4Line /> 
-            </span>
+            <span><RiMenuUnfold4Line /></span>
 
+            <span>GetDone</span>
 
+            <span><IoSettingsSharp /></span>
         </header>
     );
 }
