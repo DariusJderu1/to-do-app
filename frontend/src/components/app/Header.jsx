3eb60f91bbdd747e22652eb568d075ec4c2f7904
Darius Jderu <jderudarius@gmail.com>
2026-07-05 13:32:38 +0300

    Add HeHeader component styling

--- frontend/src/components/app/Header.jsx
+++ frontend/src/components/app/Header.jsx
@@ -1,16 +1,25 @@
 import { RiMenuUnfold4Line } from "react-icons/ri";
 import { IoSettingsSharp } from "react-icons/io5";
+import { Link } from "react-router";
+import styles from "../../styles/app/Header.module.css";
 
 function Header() {
 
     return (
         
-        <header>
-            <span><RiMenuUnfold4Line /></span>
+        <header className={styles.headerLayout}>
+            <button className={styles.iconButton} aria-label="Open navigation menu">
+                <RiMenuUnfold4Line />
+            </button>
+            
+            <Link className={styles.logo} to="/">
+                <span>Get</span>
+                <span className={styles.logoAccent}>Done</span>
+            </Link>
 
-            <span>GetDone</span>
-
-            <span><IoSettingsSharp /></span>
+            <button className={`${styles.iconButton} ${styles.settingsButton}`} aria-label="Open settings">
+                <IoSettingsSharp />
+            </button>
         </header>
     );
 }
