062e8e5d8868922ebc5d531212a662c76e5f69a1
Darius Jderu <jderudarius@gmail.com>
2026-07-05 12:42:32 +0300

    Add App component base styles

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,11 +1,12 @@
 import Header from "./app/Header.jsx";
 import Sidebar from "./app/Sidebar.jsx";
+import styles from "../styles/App.module.css";
 
 function App() {
     
     return (
 
-        <div>
+        <div className={styles.appLayout}>
             <Header />
 
             <Sidebar />
