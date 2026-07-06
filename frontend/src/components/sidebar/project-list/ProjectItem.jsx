3a6a53b3dca8aa5ab238fa4bb043efde176a62a4
Darius Jderu <jderudarius@gmail.com>
2026-07-06 18:37:58 +0300

    Add ProjectItem component

--- frontend/src/components/sidebar/project-list/ProjectItem.jsx
+++ frontend/src/components/sidebar/project-list/ProjectItem.jsx
@@ -1,8 +1,18 @@
-function ProjectItem() {
+import { FaArrowUpRightDots } from "react-icons/fa6";
+import { BsThreeDots } from "react-icons/bs";
+
+function ProjectItem({projectData}) {
 
     return (
 
-        <li></li>
+        <li>
+            <div>
+                <span><FaArrowUpRightDots /></span>
+                <span>{projectData.name}</span>
+            </div>
+
+            <span><BsThreeDots /></span>
+        </li>
     );
 }
 
