0c941d285e41d9364bc55379b4b5474e5bd47525
Darius Jderu <jderudarius@gmail.com>
2026-07-07 17:05:39 +0300

    Add styles for AddProjectForm component

--- frontend/src/components/sidebar/AddProjectForm.jsx
+++ frontend/src/components/sidebar/AddProjectForm.jsx
@@ -1,18 +1,31 @@
 import { FaArrowUpRightDots } from "react-icons/fa6";
 import AddButton from "../ui/AddButton.jsx";
 import CancelButton from "../ui/CancelButton.jsx";
+import styles from "../../styles/sidebar/AddProjectForm.module.css";
 
 function AddProjectForm({handleOpenForm, openForm}) {
 
     return (
 
-        <div>
-            <span><FaArrowUpRightDots /></span>
+        <div className={styles.addProjectForm}>
+            <span className={styles.projectIcon} aria-hidden="true">
+                <FaArrowUpRightDots />
+            </span>
 
-            <form>
-                <input type="text" />
+            <form className={styles.form}>
+                <input 
+                    className={styles.projectNameInput}
+                    type="text" 
+                    name="projectName"
+                    placeholder="Enter Project Name"
+                    aria-label="Project name"
+                    autoComplete="off"
+                    maxLength={50}
+                    required
+                    autoFocus
+                />
 
-                <div>
+                <div className={styles.formActions}>
                     <AddButton />
                     <CancelButton handleOpenForm={handleOpenForm} openForm={openForm} />
                 </div>
