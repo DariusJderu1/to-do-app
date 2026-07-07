6a4cc042918e1246ccda68e63238cad720af9858
Darius Jderu <jderudarius@gmail.com>
2026-07-07 13:26:38 +0300

    Add styles for ProjectItem component

--- frontend/src/components/sidebar/project-list/ProjectItem.jsx
+++ frontend/src/components/sidebar/project-list/ProjectItem.jsx
@@ -1,17 +1,25 @@
 import { FaArrowUpRightDots } from "react-icons/fa6";
 import { BsThreeDots } from "react-icons/bs";
+import styles from "../../../styles/sidebar/project-list/ProjectItem.module.css";
 
 function ProjectItem({projectData}) {
 
     return (
 
-        <li>
-            <div>
-                <span><FaArrowUpRightDots /></span>
-                <span>{projectData.name}</span>
+        <li className={styles.projectItem}>
+            <div className={styles.projectInformation}>
+                <span className={styles.projectIcon} aria-hidden="true">
+                    <FaArrowUpRightDots />
+                </span>
+
+                <span className={styles.projectName}>
+                    {projectData.name}
+                </span>
             </div>
 
-            <span><BsThreeDots /></span>
+            <button className={styles.menuButton} aria-label={`Open options for ${projectData.name}`}>
+                <BsThreeDots aria-hidden="true" />
+            </button>
         </li>
     );
 }
