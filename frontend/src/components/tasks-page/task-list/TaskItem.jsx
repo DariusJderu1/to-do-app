27efc6e909ce5c3aaf4c3407bdff5341c83ef278
Darius Jderu <jderudarius@gmail.com>
2026-07-11 17:52:36 +0300

    Add stylles for TaskItem component

--- frontend/src/components/tasks-page/task-list/TaskItem.jsx
+++ frontend/src/components/tasks-page/task-list/TaskItem.jsx
@@ -1,27 +1,47 @@
 import { FaRegStar, FaStar } from "react-icons/fa";
 import ActionsMenu from "../../ui/ActionsMenu.jsx";
+import styles from "../../../styles/tasks-page/task-list/TaskItem.module.css";
 
 function TaskItem({taskData, openMenuTaskId, setOpenMenuTaskId}) {
 
     return (
 
-        <li>
-            <input type="checkbox" value="true"/>
+        <li className={styles.taskItem}>
+            <div className={styles.taskMainContent}>
+                <input 
+                    className={styles.taskCheckbox}
+                    type="checkbox" 
+                    value="true"
+                    aria-label={`Mark ${taskData.title} as completed`}
+                />
+
+                <div className={styles.taskInformation}> 
+                    <span className={styles.taskTitle}>{taskData.title}</span>
+
+                    {taskData.description ?
+                        <span className={styles.taskDescription}>{taskData.description}</span> :
+                        null}
+                </div>
+            </div>
 
             <div>
-                <div>
-                    <span>{taskData.dueDate}</span>
+                <span>{taskData.dueDate || "No due date"}</span>
 
+                <span
+                    className={`${styles.starIcon} ${
+                        taskData.completed ? styles.inactiveStar : styles.activeStar
+                    }`}
+                >
                     {taskData.completed ? 
-                        <FaRegStar /> :
-                        <FaStar />}
+                    <FaRegStar /> :
+                    <FaStar />}
+                </span>
 
-                    <ActionsMenu 
-                        currentId={taskData.id}
-                        openMenuId={openMenuTaskId}
-                        setOpenMenuId={setOpenMenuTaskId}
-                    />
-                </div>
+                <ActionsMenu 
+                    currentId={taskData.id}
+                    openMenuId={openMenuTaskId}
+                    setOpenMenuId={setOpenMenuTaskId}
+                />
             </div>
         </li>
     );
