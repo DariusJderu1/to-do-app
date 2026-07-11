669ed8cab2143ce82de475b44b2a6c1411cef78b
Darius Jderu <jderudarius@gmail.com>
2026-07-11 18:19:45 +0300

    Update todos.js

--- frontend/src/components/app/api/projects.js
+++ frontend/src/components/app/api/projects.js
@@ -1,22 +1,3 @@
-export async function getDeleteProjectApiResponseBody(projectId) {
-
-    const completeUrl = "http://localhost:8080/api/projects" + "/" + projectId;
-    const response = await fetch(completeUrl, {
-
-        method: "DELETE"
-    });
-
-    if(!response.ok) {
-
-        const errorData = await response.text();
-        throw new Error(errorData);
-    }
-
-    const data = await response.text();
-    
-    return data;
-}
-
 export async function getAddProjectApiResponseBody(projectName) {
 
     const response = await fetch("http://localhost:8080/api/projects", {
@@ -56,11 +37,29 @@ export async function getUpdateProjectApiResponseBody(projectId, newProjectName)
     if(!response.ok) {
 
         const errorData = await response.json();
-
         throw new Error(errorData.message);
     }
 
     const data = await response.json();
 
+    return data;
+}
+
+export async function getDeleteProjectApiResponseBody(projectId) {
+
+    const completeUrl = "http://localhost:8080/api/projects" + "/" + projectId;
+    const response = await fetch(completeUrl, {
+
+        method: "DELETE"
+    });
+
+    if(!response.ok) {
+
+        const errorData = await response.text();
+        throw new Error(errorData);
+    }
+
+    const data = await response.text();
+    
     return data;
 }
---
+++ frontend/src/components/app/api/todos.js
@@ -0,0 +1,23 @@
+export async function getPatchTodoApiResponseBody(todoId, updatedTodoPatchBody) {
+
+    const completeUrl = "http://localhost:8080/api/todos" + todoId;
+
+    const response = await fetch(completeUrl, {
+
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedTodoPatchBody);
+    });
+
+    if(!response.ok) {
+
+        const errorData = await response.json();
+        throw new Error(errorData.message);
+    }
+
+    const data = await response.json();
+
+    return data;
+}
